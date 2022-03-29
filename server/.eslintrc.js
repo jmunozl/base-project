8f7fcba7cb038b883295c2cf47c05b55f3c3d96b
Jose Munoz <jos.munozl@gmail.com>
2022-03-29 00:18:38 -0300

    Init NestJs

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,14 +1,26 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: ['standard', 'eslint-config-prettier'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		ecmaVersion: 'latest',
+		project: 'tsconfig.json',
 		sourceType: 'module',
 	},
-	plugins: ['@typescript-eslint'],
-	rules: {},
+	plugins: ['@typescript-eslint/eslint-plugin'],
+	extends: [
+		'eslint-config-prettier',
+		'@typescript-eslint/parser',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	ignorePatterns: ['.eslintrc.js'],
+	rules: {
+		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+	},
 }
